04346a78d088fedf081d204563e5762b89e86f2b
rckrieger <rebecca.c.krieger@gmail.com>
2025-07-01 01:13:06 -0700

    Initial Commit

---
+++ test gameTests/test_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_gameTests.swift
+//  test gameTests
+//
+//  Created by Rebecca Krieger on 7/1/25.
+//
+
+import Testing
+@testable import test_game
+
+struct test_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
